7b4c17839afdc2233ef2dba315a7272a42b152cb
Tayyab Waqar <tayyab.waqar@ibm.com>
2020-03-03 11:37:42 -0500

    Add map

---
+++ my-app/src/components/Map.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+function Map() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken='pk.eyJ1IjoidGF5eWFiZGV2IiwiYSI6ImNrN2M0NW13ODBoMnIzbHFoYmR1Y2ZjdDYifQ.jNx0hMccnFqRtzvck4PLJQ'
+    />
+  );
+}
+
+export default Map
